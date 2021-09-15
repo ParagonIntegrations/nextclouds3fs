3bc90e809b8dcca6942998c6637ab14cffe0ea7c
Nelis Volschenk <nelisvolschenk@gmail.com>
2021-09-15 16:35:59 +0200

    fixed parent path

--- restart.sh
+++ restart.sh
@@ -2,7 +2,7 @@
 
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-cd parent_path
+cd $parent_path
 docker-compose down
 rm -R $parent_path/datadir/nextcloud/bucket/data
 docker-compose up -d
