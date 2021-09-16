5bb7a5e97aa05c3bd3a058391c37e46faa8401f3
Nelis Volschenk <nelisvolschenk@gmail.com>
2021-09-16 08:45:38 +0200

    added verbose flag for when running manually

--- restart.sh
+++ restart.sh
@@ -4,5 +4,5 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 cd $parent_path
 docker-compose down
-rm -R $parent_path/datadir/nextcloud/bucket/data
+rm -R --verbose $parent_path/datadir/nextcloud/bucket/data
 docker-compose up -d
