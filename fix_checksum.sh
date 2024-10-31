bbae70c187ae39c834ded04ef5808fd60204b009
Nelis Volschenk <nelisvolschenk@gmail.com>
2024-10-31 14:19:56 +0200

    Fixed so script can run even with special characters in passwords

--- fix_checksum.sh
+++ fix_checksum.sh
@@ -1,3 +1,8 @@
-cd datadir
+#!/bin/bash
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+cd $parent_path/datadir
 sudo tar czpvf mariadb.tar.gz mariadb
-mysql -u root -p -e "UPDATE oc_filecache SET checksum = '' WHERE COALESCE (checksum, '') <> ''" nextcloud
+cd $parent_path
+export $(cat db.env | xargs) && docker exec -it mariadb mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "UPDATE oc_filecache SET checksum = '' WHERE COALESCE (checksum, '') <> ''" nextcloud
